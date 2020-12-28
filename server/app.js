dc59a6fa2a1a7751ee95f3d538e6ef2892383420
TheNear <thenearx@gmail.com>
2020-12-28 23:58:52 +0300

    add dotenv

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const resolvers = {
 }
 
 // END: MOCKS
-
+require("dotenv").config();
 const app = express();
 const PORT = 8080;
 const apollo = new ApolloServer({ typeDefs, resolvers });
@@ -32,5 +32,5 @@ app.get("*", (req, res) => {
 
 
 app.listen(PORT, () => {
-  console.log(`Server started on PORT: ${PORT}`);
+  console.log(`Server started ${process.env.TEST_END} on PORT: ${PORT}`);
 });
