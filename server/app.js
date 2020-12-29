4c6903396cb455ad10bfb54ea82379d9394a57a9
TheNear <thenearx@gmail.com>
2020-12-29 12:47:08 +0300

    add mongoose

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const { ApolloServer, gql  } = require("apollo-server-express");
-
+const mongoose = require("mongoose");
 // MOCKS
 const typeDefs = gql`
   type Query {
@@ -15,22 +15,30 @@ const resolvers = {
     hello: () => "Hello world!"
   },
 }
-
 // END: MOCKS
+mongoose.connect(process.env.MONGO_KEY, { useNewUrlParser: true , useUnifiedTopology: true })
+
 const app = express();
+const dbConnect = mongoose.connection;
 const PORT = 8080;
 const apollo = new ApolloServer({ typeDefs, resolvers });
 
 apollo.applyMiddleware({ app })
 app.use(express.json());
-// app.use()
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("*", (req, res) => {
+app.get("*", (_, res) => {
   res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
+dbConnect.on("error", (err) => {
+  console.log(`DB connection failed with error: ${err}`);
+});
+
+dbConnect.once("open", () => {
+  console.log("DB connection successful");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server started ${process.env.TEST_END} on PORT: ${PORT}`);
+  console.log(`Server started on PORT: ${PORT}`);
 });
