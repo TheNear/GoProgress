da753b36bbdec95e0ed554d4272f3c088b6ab925
TheNear <thenearx@gmail.com>
2021-01-22 18:36:47 +0300

    Add cors

--- server/core/server.js
+++ server/core/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { PORT } = require("./config");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
 const serverConnect = async () => {
   const buildPath = path.join(__dirname, "../../build");
   const indexPath = path.join(__dirname, "../../build/index.html")
-  
+  app.use(cors());
   app.use(express.json());
   app.use(express.static(buildPath));
   app.get("*", (_, res) => res.sendFile(indexPath));
